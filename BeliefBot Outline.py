99d587984681910dc51c8f39e675709626f0f9a2
Brady G <bradypgarrison@gmail.com>
2022-03-27 13:26:28 -0400

    Made functions from Wang et al in python pseudocode

--- BeliefBot Outline.py
+++ BeliefBot Outline.py
@@ -35,3 +35,155 @@ class MHTBot():
     def handle_game_end(self, winner_color: Optional[Color], win_reason: Optional[WinReason],
                         game_history: GameHistory):
         
+    
+
+        
+
+        """
+Broot , maximal samplings T, maximal iterations S
+1: function BS-MCTS Broot
+2: t ← 1
+3: repeat
+4: γ ← Sampling(Broot)
+5: s ← 1
+6: repeat
+7: R ← Search(γ,Broot)
+8: N(γ) ← N(γ) + 1
+9: s ← s + 1
+10: until s > S
+11: t ← t + 1
+12: until t > T
+13: return a ← argmaxa∈A(Br o o t )U(Broot, a)
+14: end function
+
+"""
+def BSMCTS(root_node, max_samples, max_iterations):
+    t = 1
+    while (t < max_samples):
+        belief = sampling(root_node)
+        s = 1
+        
+        while (s < max_iterations):
+            reward = search(belief, root_node)
+            belief.visits += 1
+            s += 1
+        
+        t += 1
+    
+    
+    action = maxRewardAction(root_node)
+    
+    return action
+    
+
+"""
+
+15: function Expansion γ, B
+16: N(γ) ← 0
+17: for all a ∈ A(γ) do
+18: if B · a not in the tree then
+19: add B · a to the tree
+20: end if
+21: if γ · a not in B · a then
+22: add γ · a to B · a
+23: N(γ, a) ← 0
+24: U(γ, a) ← 0
+25: end if
+26: end for
+27: end function
+"""
+
+def expansion(belief, node):
+    
+    belief.visits = 0
+    
+    for action in belief.actions:
+        if nodeTakeAction(node, action) not in tree:
+            node.children.append(nodeTakeAction(node, action))
+            
+        if beliefTakeAction(belief, action) not in nodeTakeAction(node, action):
+            nodeTakeAction(node, action).addBelief(beliefTakeAction(belief, action))
+            beliefTakeAction(belief, action).reward = 0
+            beliefTakeAction(belief, action).visits = 0
+            
+
+"""
+
+28: function SamplingBroot
+29: generate new γ
+30: add γ to Broot
+31: end function
+"""            
+
+def sampling(root_node):
+    belief = generateBelief()
+    root_node.addBelief(belief)
+    
+"""
+
+32: function Search γ,B
+33: if N(B) = 0 then
+34: R ← Simulation(γ)
+35: return R
+36: end if
+37: if γ has no children then
+38: Expansion(γ,B)
+39: end if
+40: N(γ) ← N(γ) + 1
+41: action a← Selection(γ,B)
+42: R←−Search(γ · a,B · a)
+43: N(γ, a) ← N(γ, a) + 1
+44: U(γ, a) ← U(γ, a) + 1
+N (γ ,a) [R − U(γ, a)]
+45: return R
+46: end function
+
+"""
+
+def search(belief, node):
+    if (node.visits == 0):
+        reward = simulation(belief)
+        return reward
+    
+    if (node.children == []):
+        expansion(belief, node)
+        
+    belief.visits += 1
+    action = selection(belief, node)
+    reward = search(beliefTakeAction(belief, action), nodeTakeAction(node, action))
+    
+    belief.actionVisits[action] += 1
+    
+    belief.actionRewards[action] += (1/belief.actionVisits[action]) * (reward - belief.actionRewards[action])
+    
+    return reward
+
+"""
+47: function Selectionγ, B
+48: if B is Player node then
+49: a ← argmaxa∈A(γ )VplayerNode (B, a)
+50: else
+51: a ← RouletteWheelSelection(Pro(ai))
+52: end if
+53: return a
+54: end function
+"""
+
+def selection(belief, node):
+    
+    if node.isPlayerNode():
+        action = nodeRewardEstimation(node, belief, action)
+        
+    else:
+        action = rouleteWheelSelection(actionProbabilities(node))
+
+    
+    
+
+
+
+
+
+
+
+
